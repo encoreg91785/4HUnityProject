c7a949ad5040475bb21149a4a59f70f474d8614d
Liyi_Zheng <m10203407e@gmail.com>
2019-06-29 13:56:04 +0800

    Add LevelBoardUI

---
+++ Assets/Scripts/Item/LevelItem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class LevelItem : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    public bool active;
+    public Action pointerDown;
+    //[HideInInspector]
+    public Level level;
+    public GameObject frame;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!active) return;
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 targetPos = new Vector3(mousePos.x - Screen.width * 0.5f, mousePos.y - Screen.height * 0.5f, mousePos.z);
+        level.position = targetPos;
+        transform.localPosition = targetPos;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!active && pointerDown == null) return;
+        pointerDown();
+    }
+
+    public void SetFocus(bool focus)
+    {
+        frame.SetActive(focus);
+    }
+}
